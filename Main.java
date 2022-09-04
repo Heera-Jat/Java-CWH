e795bc1960d6e8eeb9742ea2ceb2560984440b6a
Heera Jat <106680359+Heera-Jat@users.noreply.github.com>
2022-09-04 10:58:56 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,52 @@
-public class Main 
+import java.util.Scanner;
+public class Main
 {
-    public static void main(String[] args) 
-    {
-        // 1. Arithmetic Operators
-        int a = 14;
-        int b = a+3;
-        System.out.println(b);
+	public static void main(String[] args) 
+	{
+	    //new tax System
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your income in Lakhs per annum");
         
-        int c = 19;
-        int d = c%3;    //modulo Operator
-        System.out.println(d);
+        float tax = 0;
+        float income = sc.nextFloat();
         
-        double f=3.4;
-        double g=f%1.1;          //Returns Decimal Remainder
-        System.out.println(g);
+        if(income<=2.5){
+          tax = tax + 0;
+        }
+        else if(income>2.5f && income <= 5f){
+            tax = tax + 0.05f * (income - 2.5f);
+        }
+        else if(income>5f && income <= 7.5f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.1f * (income - 5f);
+        }
+        else if(income>7.5f && income <= 10.0f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.1f * (7.5f - 5f);
+            tax = tax + 0.15f*(income -7.5f);
+        }
+        else if(income>10.0f && income <= 12.5f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.1f * (7.5f - 5f);
+            tax = tax + 0.15f*(10f - 7.5f);
+            tax = tax + 0.2f*(income - 10f);
+        }
+        else if(income>12.5f && income <= 15f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.1f * (7.5f - 5f);
+            tax = tax + 0.15f*(10f -7.5f);
+            tax = tax + 0.2f*(12.5f - 10f);
+            tax = tax + 0.25f*(income - 12.5f);
+        }
+        else if(income>15f){
+            tax = tax + 0.05f * (5.0f - 2.5f);
+            tax = tax + 0.1f * (7.5f - 5f);
+            tax = tax + 0.15f*(10f -7.5f);
+            tax = tax + 0.2f*(12.5f - 10f);
+            tax = tax + 0.25f*(15f - 12.5f);
+            tax = tax + 0.3f*(income - 15f);
+        }
         
-        // 2. Assignment Operators
-        int e = 9;
-            e*= 3;
-        System.out.println(e);
-
-        // 3. Comparison Operators
-         System.out.println(64<60);
-
-        // 4. Logical Operators
-        System.out.println(64>5 && 64>98);
-        System.out.println(64>5 || 64>98);
-
-        // 5. Bitwise Operators
-        System.out.println(2&3);
-        System.out.println(1&1);
-        System.out.println(0&1);
-        System.out.println(1&3);
-        
-    }
+        System.out.println("The total tax paid by the employee is: " + tax);
+	}
 }
